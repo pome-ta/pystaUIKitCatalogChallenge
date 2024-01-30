076878e04786371cab633ac8ee3ba027654181b3
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-30 17:25:26 +0900

    meta class

--- main.py
+++ main.py
@@ -95,6 +95,176 @@ class ObjcMetaView:
 UINavigationController = ObjCClass("UINavigationController")
 UINavigationBarAppearance = ObjCClass("UINavigationBarAppearance")
 
+
+class ObjcNavigationController(ObjcController):
+
+  def _override_controller(self):
+    # --- `UINavigationController` Methods
+
+    # --- `UINavigationController` set up
+    _methods = []
+    if self._msgs: _methods.extend(self._msgs)
+
+    create_kwargs = {
+      'name': '_nv',
+      'superclass': UINavigationController,
+      'methods': _methods,
+    }
+    _nv = create_objc_class(**create_kwargs)
+    self.controller_instance = _nv
+
+  def willShowViewController(self,
+                             navigationController: UINavigationController,
+                             viewController: UIViewController, animated: bool):
+
+    pass
+
+  def create_navigationControllerDelegate(self):
+    # --- `UINavigationControllerDelegate` Methods
+    def navigationController_willShowViewController_animated_(
+        _self, _cmd, _navigationController, _viewController, _animated):
+
+      navigationController = ObjCInstance(_navigationController)
+      viewController = ObjCInstance(_viewController)
+      self.willShowViewController(navigationController, viewController,
+                                  _animated)
+
+    # --- `UINavigationControllerDelegate` set up
+    _methods = [
+      navigationController_willShowViewController_animated_,
+    ]
+    _protocols = [
+      'UINavigationControllerDelegate',
+    ]
+
+    create_kwargs = {
+      'name': '_nvDelegate',
+      'methods': _methods,
+      'protocols': _protocols,
+    }
+    _nvDelegate = create_objc_class(**create_kwargs)
+    return _nvDelegate.new()
+
+  def _init_controller(self,
+                       vc: UIViewController,
+                       is_main_thread: bool = False):
+
+    def __initialize():
+      self._override_controller()
+      _delegate = self.create_navigationControllerDelegate()
+      nv = self.controller_instance.alloc()
+      nv.initWithRootViewController_(vc).autorelease()
+      nv.setDelegate_(_delegate)
+      return nv
+
+    if is_main_thread:
+
+      @on_main_thread
+      def __run():
+        return __initialize()
+    else:
+
+      def __run():
+        return __initialize()
+
+    return __run()
+
+  @classmethod
+  def new(cls,
+          viewController: UIViewController,
+          is_main_thread: bool = False) -> ObjCInstance:
+    _cls = cls()
+    return _cls._init_controller(viewController, is_main_thread)
+
+
+UIViewController = ObjCClass('UIViewController')
+
+
+class ObjcViewController(ObjcController):
+
+  def didLoad(self, this: UIViewController):
+    pass
+
+  def willAppear(self, this: UIViewController, animated: bool):
+    pass
+
+  def willLayoutSubviews(self, this: UIViewController):
+    pass
+
+  def didLayoutSubviews(self, this: UIViewController):
+    pass
+
+  def didAppear(self, this: UIViewController, animated: bool):
+    pass
+
+  def willDisappear(self, this: UIViewController, animated: bool):
+    pass
+
+  def didDisappear(self, this: UIViewController, animated: bool):
+    pass
+
+  def _override_controller(self):
+    # --- `UIViewController` Methods
+    def viewDidLoad(_self, _cmd):
+      this = ObjCInstance(_self)
+      self.didLoad(this)
+
+    def viewWillAppear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.willAppear(this, animated)
+
+    def viewWillLayoutSubviews(_self, _cmd):
+      this = ObjCInstance(_self)
+      self.willLayoutSubviews(this)
+
+    def viewDidLayoutSubviews(_self, _cmd):
+      this = ObjCInstance(_self)
+      self.didLayoutSubviews(this)
+
+    def viewDidAppear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.didAppear(this, animated)
+
+    def viewWillDisappear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.willDisappear(this, animated)
+
+    def viewDidDisappear_(_self, _cmd, animated):
+      this = ObjCInstance(_self)
+      self.didDisappear(this, animated)
+
+    # --- `UIViewController` set up
+    _methods = [
+      viewDidLoad,
+      viewWillAppear_,
+      viewWillLayoutSubviews,
+      viewDidLayoutSubviews,
+      viewDidAppear_,
+      viewWillDisappear_,
+      viewDidDisappear_,
+    ]
+
+    if self._msgs: _methods.extend(self._msgs)
+
+    create_kwargs = {
+      'name': '_vc',
+      'superclass': UIViewController,
+      'methods': _methods,
+    }
+    _vc = create_objc_class(**create_kwargs)
+    self.controller_instance = _vc
+
+  def _init_controller(self):
+    self._override_controller()
+    vc = self.controller_instance.new().autorelease()
+    return vc
+
+  @classmethod
+  def new(cls, *args, **kwargs) -> ObjCInstance:
+    _cls = cls(*args, **kwargs)
+    return _cls._init_controller()
+
+
 if __name__ == "__main__":
   pass
 
