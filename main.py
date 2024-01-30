27b30d408a006484782c6c7178184ca5157c68af
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-30 13:57:32 +0900

    meta class

--- main.py
+++ main.py
@@ -23,20 +23,77 @@ class UIModalPresentationStyle:
 
 
 @on_main_thread
-def present_objc(viewController):
+def run_controller(view_controller):
   app = ObjCClass("UIApplication").sharedApplication()
   window = app.keyWindow() if app.keyWindow() else app.windows().firstObject()
 
-  rootViewController = window.rootViewController()
+  root_view_controller = window.rootViewController()
 
-  while rootViewController.presentedViewController():
-    rootViewController = rootViewController.presentedViewController()
+  while root_view_controller.presentedViewController():
+    root_view_controller = root_view_controller.presentedViewController()
 
-  fullScreen = UIModalPresentationStyle.fullScreen
-  viewController.setModalPresentationStyle_(fullScreen)
-  rootViewController.presentViewController_animated_completion_(
-    viewController, True, None)
+  full_screen = UIModalPresentationStyle.fullScreen
+  view_controller.setModalPresentationStyle_(full_screen)
+  root_view_controller.presentViewController_animated_completion_(
+    view_controller, True, None)
 
+
+class ObjcController:
+
+  def __init__(self, *args, **kwargs):
+    self._msgs: list['def'] = []  # xxx: 型名ちゃんとやる
+    self.controller_instance: ObjCInstance
+
+  def override(self):
+    # todo: objc で独自にmethod 生やしたいときなど
+    # todo: この関数内に関数を作り`@self.add_msg`
+    pass
+
+  def add_msg(self, msg):
+    if not (hasattr(self, '_msgs')):
+      self._msgs: list['def'] = []
+    self._msgs.append(msg)
+
+  def _override_controller(self):
+    # todo: 既存method と独自追加method をシュッと持ちたい
+    # if self._msgs: _methods.extend(self._msgs)
+    pass
+
+  def _init_controller(self):
+    pass
+
+  @classmethod
+  def new(cls) -> ObjCInstance | None:
+    return None
+
+
+class ObjcMetaView:
+
+  def __init__(self, *args, **kwargs):
+    self.instance: ObjCInstance
+    self.CGRectZero = CGRect((0.0, 0.0), (0.0, 0.0))
+
+    self.IS_LAYOUT_DEBUG = True if kwargs.get('LAYOUT_DEBUG') else False
+
+  def _init(self):
+    if self.IS_LAYOUT_DEBUG:
+      from objc_util import ObjCClass
+      color = ObjCClass('UIColor').systemRedColor()
+      self.instance.layer().setBorderWidth_(1.0)
+      self.instance.layer().setBorderColor_(color.cgColor())
+    self.instance.setTranslatesAutoresizingMaskIntoConstraints_(False)
+
+    return self.instance
+
+  @classmethod
+  def new(cls, *args, **kwargs) -> ObjCInstance:
+    _cls = cls(*args, **kwargs)
+    return _cls._init()
+
+
+# --- navigation
+UINavigationController = ObjCClass("UINavigationController")
+UINavigationBarAppearance = ObjCClass("UINavigationBarAppearance")
 
 if __name__ == "__main__":
   pass
