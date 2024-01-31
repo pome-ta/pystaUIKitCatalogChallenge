eec0224d714dbe0f522a8b11f3464bfbae49b5ca
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-01-31 15:29:27 +0900

    _controller.py

---
+++ objcista/_controller.py
@@ -0,0 +1,30 @@
+from objc_util import ObjCInstance
+
+
+class _Controller:
+
+  def __init__(self, *args, **kwargs):
+    self._msgs: list['def'] = []  # xxx: 型名ちゃんとやる
+    self.controller_instance: ObjCInstance
+
+  def override(self):
+    # todo: objc で独自にmethod 生やしたいときなど
+    # todo: この関数内に関数を作り`@self.add_msg`
+    pass
+
+  def add_msg(self, msg):
+    if not (hasattr(self, '_msgs')):
+      self._msgs: list['def'] = []
+    self._msgs.append(msg)
+
+  def _override_controller(self):
+    # todo: 既存method と独自追加method をシュッと持ちたい
+    # if self._msgs: _methods.extend(self._msgs)
+    pass
+
+  def _init_controller(self):
+    pass
+
+  @classmethod
+  def new(cls) -> ObjCInstance | None:
+    return None
