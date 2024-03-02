81b3d8299dea9eaf07c9dfa40a9e52c9904aae4b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-03-02 13:18:48 +0900

    set dict

--- baseTableViewController.py
+++ baseTableViewController.py
@@ -6,14 +6,20 @@ localizable_url = './UIKitCatalogCreatingAndCustomizingViewsAndControls/UIKitCat
 localizable_path = Path(localizable_url)
 '''
 localizable_txt = localizable_path.read_text(encoding='utf-8')
-
 localizable_plane_list = localizable_txt.splitlines()
-
 '''
 
-localizable_plane_list = localizable_path.read_text(
-  encoding='utf-8').splitlines()
+splitlines_list = localizable_path.read_text(encoding='utf-8').splitlines()
+pre_processing = [
+  line for line in splitlines_list
+  if len(line) and (line[0] == '"') and (line[-1] == ';')
+]
 
 compile = re.compile('"(.*?)"')
-[print(compile.findall(s)) for s in localizable_plane_list]
+localizable_dic = dict([
+  reg_result for line in pre_processing
+  if (reg_result := compile.findall(line))
+])
+
+
 
