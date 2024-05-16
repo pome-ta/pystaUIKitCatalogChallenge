a78e9ce2d3f8b8cb1bd6c58e7ec0c0fc110cda57
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-05-17 00:25:02 +0900

    add

---
+++ caseElement.py
@@ -0,0 +1,15 @@
+class CaseElement:
+
+  def __init__(self, title: str, cellID: str, configHandler):
+    # セルの視覚的なタイトル (テーブル セクションのヘッダー タイトル)
+    self.title = title
+    # nib ファイル内でセルを検索するためのテーブルビューのセルの識別子。
+    self.cellID = cellID
+    # セルのサブビューを設定するための構成ハンドラー。
+    # xxx: ガバガバ
+    self.configHandler = configHandler
+
+  @staticmethod
+  def targetView(cell):
+    return cell.contentView.subviews[0] if cell != None else None
+
