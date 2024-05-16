4ae6fc2c9b9cf5c8aeb4af5f02a49375809f0872
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-05-16 23:39:57 +0900

    args

--- storyboard/buttonViewController.py
+++ storyboard/buttonViewController.py
@@ -6,15 +6,10 @@ from pyrubicon.objc.types import NSInteger
 
 from rbedge.enumerations import UIButtonType, UIControlState
 
-from rbedge import pdbr
-
 #ObjCClass.auto_rename = True # xxx: ここ含めて全部呼び出し？
 
 UITableViewCell = ObjCClass('UITableViewCell')
-
 UIButton = ObjCClass('UIButton')
-UIButtonConfiguration = ObjCClass('UIButtonConfiguration')
-
 NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
 
 prototypes: list[dict[UITableViewCell, str]] = []
@@ -36,18 +31,20 @@ class CustomTableViewCell(UITableViewCell):
   @objc_method
   def initWithStyle_reuseIdentifier_(self, style: NSInteger, reuseIdentifier):
 
-    super_args = []
+    super_args = [
+      style,
+      reuseIdentifier,
+    ]
+    super_argtypes = [
+      NSInteger,
+      ctypes.c_void_p,
+    ]
 
     self_ptr = send_super(__class__,
                           self,
                           'initWithStyle:reuseIdentifier:',
-                          style,
-                          reuseIdentifier,
-                          argtypes=[
-                            NSInteger,
-                            ctypes.c_void_p,
-                          ])
-
+                          *super_args,
+                          argtypes=super_argtypes)
     # todo: `self` に再定義しない
     #self = ObjCInstance(self_ptr)
     self.overrideCell()
@@ -472,6 +469,46 @@ class ButtonStyleTinted(CustomTableViewCell):
 
 @add_prototype('buttonStyleFilled')
 class ButtonStyleFilled(CustomTableViewCell):
+
+  @objc_method
+  def overrideCell(self):
+    type = UIButtonType.system
+    button = UIButton.buttonWithType_(type)
+
+    title = 'Button'
+    state = UIControlState.normal
+    button.setTitle_forState_(title, state)
+
+    button.translatesAutoresizingMaskIntoConstraints = False
+
+    contentView = self.contentView
+    contentView.addSubview_(button)
+
+    NSLayoutConstraint.activateConstraints_([
+      button.centerXAnchor.constraintEqualToAnchor_(contentView.centerXAnchor),
+      button.centerYAnchor.constraintEqualToAnchor_(contentView.centerYAnchor),
+    ])
+
+
+@add_prototype('buttonImage')
+class ButtonImage(CustomTableViewCell):
+
+  @objc_method
+  def overrideCell(self):
+    button = UIButton.new()
+    button.translatesAutoresizingMaskIntoConstraints = False
+
+    contentView = self.contentView
+    contentView.addSubview_(button)
+
+    NSLayoutConstraint.activateConstraints_([
+      button.centerXAnchor.constraintEqualToAnchor_(contentView.centerXAnchor),
+      button.centerYAnchor.constraintEqualToAnchor_(contentView.centerYAnchor),
+    ])
+
+
+@add_prototype('buttonCornerStyle')
+class ButtonCornerStyle(CustomTableViewCell):
 
   @objc_method
   def overrideCell(self):
