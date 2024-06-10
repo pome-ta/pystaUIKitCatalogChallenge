e24fc0024857e9c4a63b08a9161815b70f7c7e76
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-06-11 00:38:25 +0900

    configureDropDownProgrammaticButton

--- menuButtonViewController.py
+++ menuButtonViewController.py
@@ -1,6 +1,9 @@
+import ctypes
 from enum import Enum
+#import re
 
-from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.api import ObjCClass, ObjCInstance, objc_method, Block
+from pyrubicon.objc.runtime import send_super
 
 from rbedge.enumerations import (
   UITableViewStyle, )
@@ -11,6 +14,11 @@ from pyLocalizedString import localizedString
 from baseTableViewController import BaseTableViewController
 from storyboard.menuButtonViewController import prototypes
 
+UIMenu = ObjCClass('UIMenu')
+UIAction = ObjCClass('UIAction')
+
+#UIMenuElement = ObjCClass('UIMenuElement')
+
 
 # Cell identifier for each menu button table view cell.
 # > 各メニュー ボタン テーブル ビュー セルのセル識別子。
@@ -19,6 +27,12 @@ class MenuButtonKind(Enum):
   buttonMenuMultiAction = 'buttonMenuMultiAction'
   buttonSubMenu = 'buttonSubMenu'
   buttonMenuSelection = 'buttonMenuSelection'
+
+
+class ButtonMenuActionIdentifiers(Enum):
+  item1 = 'item1'
+  item2 = 'item2'
+  item3 = 'item3'
 
 
 class MenuButtonViewController(BaseTableViewController):
@@ -48,6 +62,44 @@ class MenuButtonViewController(BaseTableViewController):
 
     title = NSStringFromClass(__class__)
     self.navigationItem.title = title
+
+    self.testCells.extend([
+      CaseElement(localizedString('DropDownProgTitle'),
+                  MenuButtonKind.buttonMenuProgrammatic.value,
+                  self.configureDropDownProgrammaticButton_),
+    ])
+
+  # MARK: - Handlers
+  @objc_method
+  def menuHandler_(self, _action: ctypes.c_void_p) -> None:
+    action = ObjCInstance(_action)
+    switch_case = str(action.identifier)
+    # xxx: `match` がPythonista3 (PEP8) だと、フォーマットできないため`if`
+    if switch_case == ButtonMenuActionIdentifiers.item1.value:
+      print('Menu Action: item 1')
+    elif switch_case == ButtonMenuActionIdentifiers.item2.value:
+      print('Menu Action: item 2')
+    elif switch_case == ButtonMenuActionIdentifiers.item3.value:
+      print('Menu Action: item 3')
+    else:
+      print('Menu Action: None')
+
+  # MARK: - Drop Down Menu Buttons
+  @objc_method
+  def configureDropDownProgrammaticButton_(self, button):
+    # xxx: 正規表現でやる？
+    button.menu = UIMenu.menuWithChildren_([
+      UIAction.actionWithTitle_image_identifier_handler_(
+        localizedString('ItemTitle').replace('%@', '1'), None,
+        ButtonMenuActionIdentifiers.item1.value,
+        Block(self.menuHandler_, None, ctypes.c_void_p)),
+      UIAction.actionWithTitle_image_identifier_handler_(
+        localizedString('ItemTitle').replace('%@', '2'), None,
+        ButtonMenuActionIdentifiers.item2.value,
+        Block(self.menuHandler_, None, ctypes.c_void_p)),
+    ])
+
+    button.showsMenuAsPrimaryAction = True
 
 
 if __name__ == '__main__':
