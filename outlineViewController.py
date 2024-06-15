08343375e12b67421a73914e799fd51c2c8f870a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-06-15 10:52:21 +0900

    up

--- outlineViewController.py
+++ outlineViewController.py
@@ -1,7 +1,25 @@
-from pyrubicon.objc.api import ObjCClass
+from pyrubicon.objc.api import ObjCClass, objc_method
+from pyrubicon.objc.runtime import send_super
 
+
+
+#ObjCClass.auto_rename = True
 UISplitViewController = ObjCClass('UISplitViewController')
 
+
+class OutlineViewController(UISplitViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
+
+
 if __name__ == '__main__':
-  pass
+  from rbedge.enumerations import UIModalPresentationStyle
+  from rbedge import present_viewController
+  from rbedge import pdbr
+
+  main_vc = OutlineViewController.new()
+  style = UIModalPresentationStyle.fullScreen
+  present_viewController(main_vc, style,False)
 
