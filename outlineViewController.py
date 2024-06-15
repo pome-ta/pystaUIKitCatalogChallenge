c5e04f7da39c951b82cd2cd0347ab6bdae14c23e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-06-16 01:47:39 +0900

    up

--- outlineViewController.py
+++ outlineViewController.py
@@ -4,6 +4,78 @@ from pyrubicon.objc.runtime import send_super
 #ObjCClass.auto_rename = True
 UISplitViewController = ObjCClass('UISplitViewController')
 
+from rbedge.functions import NSStringFromClass
+
+# --- UIViewController
+UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+UILabel = ObjCClass('UILabel')
+UIFont = ObjCClass('UIFont')
+UIColor = ObjCClass('UIColor')
+
+
+class FirstViewController(UIViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    # --- Navigation
+    title = NSStringFromClass(__class__)
+    self.navigationItem.title = title
+
+    # --- View
+    backgroundColor = UIColor.systemBlueColor()
+    systemBlueColor
+
+    self.view.backgroundColor = backgroundColor
+
+    self.label = UILabel.new()
+    self.label.text = title
+    self.label.font = UIFont.systemFontOfSize_(26.0)
+    self.label.sizeToFit()
+
+    self.view.addSubview_(self.label)
+
+    # --- Layout
+    self.label.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.label.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      self.label.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+    ])
+
+
+class SecondViewController(UIViewController):
+
+  @objc_method
+  def viewDidLoad(self):
+    # --- Navigation
+    title = NSStringFromClass(__class__)
+    self.navigationItem.title = title
+
+    # --- View
+    backgroundColor = UIColor.systemRedColor()
+    self.view.backgroundColor = backgroundColor
+
+    self.label = UILabel.new()
+    self.label.text = title
+    self.label.font = UIFont.systemFontOfSize_(26.0)
+    self.label.sizeToFit()
+
+    self.view.addSubview_(self.label)
+
+    # --- Layout
+    self.label.translatesAutoresizingMaskIntoConstraints = False
+
+    NSLayoutConstraint.activateConstraints_([
+      self.label.centerXAnchor.constraintEqualToAnchor_(
+        self.view.centerXAnchor),
+      self.label.centerYAnchor.constraintEqualToAnchor_(
+        self.view.centerYAnchor),
+    ])
+
 
 class OutlineViewController(UISplitViewController):
 
@@ -18,9 +90,8 @@ if __name__ == '__main__':
   from rbedge import pdbr
 
   main_vc = OutlineViewController.new()
-  
+
   #style = UIModalPresentationStyle.fullScreen
   style = UIModalPresentationStyle.pageSheet  # 1
   present_viewController(main_vc, style, False)
-  pdbr.state(main_vc)
 
