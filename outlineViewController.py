5dd27ca868e637b7109d63096f7287f947aea986
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-10-08 13:40:47 +0900

    Diffable DataSource test init

--- outlineViewController.py
+++ outlineViewController.py
@@ -1,16 +1,25 @@
+"""
+note: まずは参照してるのやりきってみる
+ref: [Diffable DataSource 入門 #Swift - Qiita](https://qiita.com/maiyama18/items/28039293b4bbf886ce8e)
+  - Diffable DataSource
+"""
+
 import ctypes
 
 from pyrubicon.objc.api import ObjCClass, objc_method
+from pyrubicon.objc.runtime import send_super
+
 from rbedge.functions import NSStringFromClass
 
 UIViewController = ObjCClass('UIViewController')
 UICollectionView = ObjCClass('UICollectionView')
 
 
-class OutlineViewController(UIViewController):
+class TodoListViewController(UIViewController):
 
   @objc_method
   def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
     # --- Navigation
     title = NSStringFromClass(__class__)
     self.navigationItem.title = title
@@ -21,7 +30,7 @@ if __name__ == '__main__':
   from rbedge import present_viewController
   from rbedge import pdbr
 
-  outline_vc = OutlineViewController.new()
+  todo_vc = TodoListViewController.new()
   style = UIModalPresentationStyle.fullScreen
-  present_viewController(outline_vc, style)
+  present_viewController(todo_vc, style)
 
