8e3a91ba6ff0fa1256d84c867bf118277494c2da
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-10-25 17:04:55 +0900

    colors

--- defaultPageControlViewController.py
+++ defaultPageControlViewController.py
@@ -7,6 +7,20 @@ from rbedge.functions import NSStringFromClass
 
 UIViewController = ObjCClass('UIViewController')
 UIPageControl = ObjCClass('UIPageControl')
+UIColor = ObjCClass('UIColor')
+
+colors = [
+  UIColor.blackColor,
+  UIColor.systemGrayColor(),
+  UIColor.systemRedColor(),
+  UIColor.systemGreenColor(),
+  UIColor.systemBlueColor(),
+  UIColor.systemPinkColor(),
+  UIColor.systemYellowColor(),
+  UIColor.systemIndigoColor(),
+  UIColor.systemOrangeColor(),
+  UIColor.systemPurpleColor(),
+]
 
 
 class PageControlViewController(UIViewController):
@@ -18,11 +32,22 @@ class PageControlViewController(UIViewController):
     title = NSStringFromClass(__class__)
     self.navigationItem.title = title
 
+    self.configurePageControl()
+
+  @objc_method
+  def configurePageControl(self):
     pageControl = UIPageControl.alloc().init().autorelease()
     pageControl.frame = CGRectMake(16, 639.5, 343, 27.5)
-    pageControl.numberOfPages = 3
-    #pdbr.state(pageControl, 1.)
-    print(pageControl.contentMode)
+
+    pageControl.setNumberOfPages_(len(colors))
+    pageControl.setCurrentPage_(2)
+    pageControl.setPageIndicatorTintColor_(UIColor.systemGreenColor())
+    pageControl.setCurrentPageIndicatorTintColor_(UIColor.systemPurpleColor())
+
+    pageControl.setBackgroundColor_(UIColor.systemDarkRedColor())
+    #pdbr.state(pageControl,1)
+    #pdbr.state(UIColor)
+
     self.view.addSubview_(pageControl)
 
 
