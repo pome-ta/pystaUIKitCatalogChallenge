bb22697a5a33ed61932619440b1fb51078a3e309
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-11-04 14:23:59 +0900

    sliderViewController.py

---
+++ sliderViewController.py
@@ -0,0 +1,64 @@
+from enum import Enum
+
+from pyrubicon.objc.api import ObjCClass, ObjCInstance, Block
+from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.runtime import SEL, send_super, objc_id
+#from pyrubicon.objc.types import CGSize, CGFloat, CGRectMake, CGSizeMake
+
+from rbedge.enumerations import UITableViewStyle
+from rbedge.functions import NSStringFromClass
+
+from caseElement import CaseElement
+from pyLocalizedString import localizedString
+
+from baseTableViewController import BaseTableViewController
+from storyboard.sliderViewController import prototypes
+
+
+class SliderKind(Enum):
+  sliderDefault = 'sliderDefault'
+  sliderTinted = 'sliderTinted'
+  sliderCustom = 'sliderCustom'
+  sliderMaxMinImage = 'sliderMaxMinImage'
+
+
+class SliderViewController(BaseTableViewController):
+
+  @objc_method
+  def init(self):
+    send_super(__class__, self, 'init')  # xxx: 不要?
+    tableViewStyle = UITableViewStyle.grouped
+    self.initWithStyle_(tableViewStyle)
+
+    self.testCells = []
+    self.initPrototype()
+
+    return self
+
+  @objc_method
+  def initPrototype(self):
+    [
+      self.tableView.registerClass_forCellReuseIdentifier_(
+        prototype['cellClass'], prototype['identifier'])
+      for prototype in prototypes
+    ]
+
+  # MARK: - View Life Cycle
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
+
+    title = NSStringFromClass(__class__)
+    self.navigationItem.title = title
+
+
+if __name__ == '__main__':
+  from rbedge.enumerations import UIModalPresentationStyle
+  from rbedge import present_viewController
+  from rbedge import pdbr
+
+  main_vc = SliderViewController.new()
+  #style = UIModalPresentationStyle.pageSheet
+  style = UIModalPresentationStyle.fullScreen
+  present_viewController(main_vc, style)
+
