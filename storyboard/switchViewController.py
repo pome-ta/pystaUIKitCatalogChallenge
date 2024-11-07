52a7cfd1a62dbba0a72a6efb60509c82c429e9e8
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-11-07 18:30:33 +0900

    switchViewController Storyboard

---
+++ storyboard/switchViewController.py
@@ -0,0 +1,80 @@
+from pyrubicon.objc.api import ObjCClass, objc_method
+from pyrubicon.objc.types import CGRectMake
+
+from ._prototype import CustomTableViewCell
+from rbedge import pdbr
+
+UISwitch = ObjCClass('UISwitch')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+
+
+def add_prototype(identifier: str):
+
+  def _create_reuse_dict(cellClass: CustomTableViewCell):
+    prototypes.append({
+      'cellClass': cellClass,
+      'identifier': identifier,
+    })
+
+  return _create_reuse_dict
+
+
+prototypes: list[dict[CustomTableViewCell, str]] = []
+
+
+@add_prototype('sliderDefault')
+class DefaultSwitch(CustomTableViewCell):
+
+  @objc_method
+  def overrideCell(self):
+    switch = UISwitch.alloc().initWithFrame_(CGRectMake(
+      163.5, 6.5, 51.0, 31.0)).autorelease()
+    
+    switch.translatesAutoresizingMaskIntoConstraints = False
+    self.contentView.addSubview_(switch)
+
+    NSLayoutConstraint.activateConstraints_([
+      switch.centerXAnchor.constraintEqualToAnchor_constant_(
+        self.contentView.centerXAnchor, -0.5),
+      switch.centerYAnchor.constraintEqualToAnchor_(
+        self.contentView.centerYAnchor),
+    ])
+
+
+@add_prototype('checkBoxSwitch')
+class CheckBoxSwitch(CustomTableViewCell):
+
+  @objc_method
+  def overrideCell(self):
+    switch = UISwitch.alloc().initWithFrame_(CGRectMake(
+      163.5, 6.5, 51.0, 31.0)).autorelease()
+    
+    switch.translatesAutoresizingMaskIntoConstraints = False
+    self.contentView.addSubview_(switch)
+
+    NSLayoutConstraint.activateConstraints_([
+      switch.centerXAnchor.constraintEqualToAnchor_constant_(
+        self.contentView.centerXAnchor, -0.5),
+      switch.centerYAnchor.constraintEqualToAnchor_(
+        self.contentView.centerYAnchor),
+    ])
+
+@add_prototype('tintedSwitch')
+class TintedSwitch(CustomTableViewCell):
+
+  @objc_method
+  def overrideCell(self):
+    switch = UISwitch.alloc().initWithFrame_(CGRectMake(
+      163.5, 6.5, 51.0, 31.0)).autorelease()
+    
+    switch.translatesAutoresizingMaskIntoConstraints = False
+    self.contentView.addSubview_(switch)
+
+    NSLayoutConstraint.activateConstraints_([
+      switch.centerXAnchor.constraintEqualToAnchor_constant_(
+        self.contentView.centerXAnchor, -0.5),
+      switch.centerYAnchor.constraintEqualToAnchor_(
+        self.contentView.centerYAnchor),
+    ])
+
+
