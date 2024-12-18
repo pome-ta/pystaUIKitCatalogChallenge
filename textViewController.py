7b527356a70d2864cfed2695b76c2b5fa11fbd6c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-12-18 15:23:14 +0900

    color named

--- textViewController.py
+++ textViewController.py
@@ -2,16 +2,22 @@
   note: Storyboard 未定義
 '''
 import ctypes
+from pathlib import Path
+import json
 
 from pyrubicon.objc.api import ObjCClass
-from pyrubicon.objc.api import objc_method
-from pyrubicon.objc.runtime import send_super
+from pyrubicon.objc.api import objc_method, objc_property, objc_const
+from pyrubicon.objc.runtime import send_super, objc_id, load_library
 
 from rbedge.enumerations import (
-  NSLineBreakMode, )
+  UIUserInterfaceStyle,
+  NSLineBreakMode,
+)
 from rbedge import pdbr
 
 from pyLocalizedString import localizedString
+
+UIKit = load_library('UIKit')  # todo: `objc_const` 用
 
 UIViewController = ObjCClass('UIViewController')
 NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
@@ -20,9 +26,47 @@ UITextView = ObjCClass('UITextView')
 UIFont = ObjCClass('UIFont')
 UIFontDescriptor = ObjCClass('UIFontDescriptor')
 
-
-
 UIColor = ObjCClass('UIColor')
+
+# --- Global Variables
+UIFontTextStyleBody = objc_const(UIKit, 'UIFontTextStyleBody')
+
+
+def get_srgb_named_style(named: str,
+                         userInterfaceStyle: UIUserInterfaceStyle) -> list:
+  # todo: 本来`UIColor.colorNamed:` で呼び出す。asset(bundle) の取り込みが難しそうなので、独自に直で呼び出し
+  _path = Path(
+    f'./UIKitCatalogCreatingAndCustomizingViewsAndControls/UIKitCatalog/Assets.xcassets/{named}.colorset/Contents.json'
+  )
+  _str = _path.read_text()
+  _dict = json.loads(_str)
+
+  def _pick_color(colors: list[dict], style: str | None = None) -> list:
+    components: dict
+    for color in colors:
+      if color.get('idiom') != 'universal':
+        continue
+      appearance, *_ = appearances if (
+        appearances := color.get('appearances')) is not None else [None]
+      if style is None and appearance is None:
+        components = color.get('color').get('components')
+        break
+      if appearance is not None and style == appearance.get('value'):
+        components = color.get('color').get('components')
+        break
+
+    red, green, blue, alpha = (float(components.get(clr))
+                               for clr in ('red', 'green', 'blue', 'alpha'))
+    # wip: エラーハンドリング
+    return [red, green, blue, alpha]
+
+  color_dicts = _dict.get('colors')
+  if userInterfaceStyle == UIUserInterfaceStyle.light:
+    return _pick_color(color_dicts, 'light')
+  elif userInterfaceStyle == UIUserInterfaceStyle.dark:
+    return _pick_color(color_dicts, 'dark')
+  else:
+    return _pick_color(color_dicts)
 
 
 class TextViewController(UIViewController):
@@ -31,7 +75,7 @@ class TextViewController(UIViewController):
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
-    self.view.backgroundColor = UIColor.systemBrownColor()  # todo: 確認用
+    self.view.backgroundColor = UIColor.systemBackgroundColor()
 
     textView = UITextView.alloc().initWithFrame_(self.view.bounds)
     textView.text = 'This is a UITextView that uses attributed text. You can programmatically modify the display of the text by making it bold, highlighted, underlined, tinted, symbols, and more. These attributes are defined in NSAttributedString.h. You can even embed attachments in an NSAttributedString!'
@@ -52,6 +96,7 @@ class TextViewController(UIViewController):
       textView.leadingAnchor.constraintEqualToAnchor_constant_(
         areaLayoutGuide.leadingAnchor, 16.0),
     ])
+    self.textView = textView
     self.configureTextView()
 
   @objc_method
@@ -77,11 +122,26 @@ class TextViewController(UIViewController):
                ])
     #pdbr.state(self.collectionView)
     #print('viewDidDisappear')
-    
-    
+
   @objc_method
   def configureTextView(self):
-    pass
+    bodyFontDescriptor = UIFontDescriptor.preferredFontDescriptorWithTextStyle_(
+      UIFontTextStyleBody)
+
+    bodyFont = UIFont.fontWithDescriptor_size_(bodyFontDescriptor, 0.0)
+    self.textView.font = bodyFont
+
+    _color_named = get_srgb_named_style(
+      'text_view_background', self.traitCollection.userInterfaceStyle)
+    self.textView.backgroundColor = UIColor.colorWithRed_green_blue_alpha_(
+      *_color_named)
+
+    self.textView.isScrollEnabled = True
+    
+    # Apply different attributes to the text (bold, tinted, underline, etc.).
+    # テキストにさまざまな属性 (太字、色付き、下線など) を適用します。
+    
+
 
 
 if __name__ == '__main__':
