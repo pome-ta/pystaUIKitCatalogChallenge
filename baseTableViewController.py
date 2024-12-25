311c966a3e3a5b2a8679d24d05dabd7076af1ecb
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-12-25 22:30:53 +0900

    initWithStyle

--- baseTableViewController.py
+++ baseTableViewController.py
@@ -1,10 +1,12 @@
-from pyrubicon.objc.api import ObjCClass, objc_method
+import ctypes
+from pyrubicon.objc.api import ObjCClass, ObjCInstance
+from pyrubicon.objc.api import objc_method
 from pyrubicon.objc.runtime import send_super, objc_id
 from pyrubicon.objc.types import NSInteger
 
 from rbedge.enumerations import UIListContentTextAlignment
 
-from caseElement import CaseElement  # todo: 型呼び出し
+from caseElement import CaseElement  # todo: 型呼び出し
 
 UITableViewController = ObjCClass('UITableViewController')
 UITableViewHeaderFooterView = ObjCClass('UITableViewHeaderFooterView')
@@ -13,19 +15,52 @@ UIListContentConfiguration = ObjCClass('UIListContentConfiguration')
 
 class BaseTableViewController(UITableViewController):
 
-  testCells: list[CaseElement] = []
+  @objc_method
+  def initWithStyle_(self, style: NSInteger) -> ObjCInstance:
+    send_super(__class__,
+               self,
+               'initWithStyle:',
+               style,
+               restype=objc_id,
+               argtypes=[
+                 NSInteger,
+               ])
+
+    self.testCells: list[CaseElement] = []
+    self.headerFooterView_identifier = 'customHeaderFooterView'
+    return self
+
+  @objc_method
+  def setupPrototypes_(self, prototypes) -> None:
+    [
+      self.tableView.registerClass_forCellReuseIdentifier_(
+        prototype['cellClass'], prototype['identifier'])
+      for prototype in prototypes
+    ]
 
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
     self.tableView.registerClass_forHeaderFooterViewReuseIdentifier_(
-      UITableViewHeaderFooterView, 'customHeaderFooterView')
+      UITableViewHeaderFooterView, self.headerFooterView_identifier)
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    # todo: `dealloc` 呼び出す為、インスタンス変数を初期化
+    self.testCells = None
+    self.headerFooterView_identifier = None
 
   @objc_method
   def centeredHeaderView_(self, title):
-    # todo: let headerView: UITableViewHeaderFooterView = UITableViewHeaderFooterView()
     headerView = self.tableView.dequeueReusableHeaderFooterViewWithIdentifier_(
-      'customHeaderFooterView')
+      self.headerFooterView_identifier)
 
     content = UIListContentConfiguration.groupedHeaderConfiguration()
     content.text = title
