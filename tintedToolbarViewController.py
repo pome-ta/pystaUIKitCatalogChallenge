ecf5b7d245a1f249c1ce760738514a3d0671d607
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-01-01 00:02:53 +0900

    init

---
+++ tintedToolbarViewController.py
@@ -0,0 +1,136 @@
+'''
+  note: Storyboard 実装なし
+'''
+import ctypes
+
+from pyrubicon.objc.api import ObjCClass, ObjCInstance, Block
+from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.runtime import send_super, objc_id, SEL
+
+from rbedge.enumerations import (
+  UIBarStyle,
+  UIBarButtonSystemItem,
+)
+
+from rbedge import pdbr
+
+UIViewController = ObjCClass('UIViewController')
+NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
+UIColor = ObjCClass('UIColor')
+
+UIBarButtonItem = ObjCClass('UIBarButtonItem')
+UIImage = ObjCClass('UIImage')
+UIMenu = ObjCClass('UIMenu')
+UIAction = ObjCClass('UIAction')
+
+
+class TintedToolbarViewController(UIViewController):
+
+  @objc_method
+  def dealloc(self):
+    # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
+    #print('\tdealloc')
+    pass
+
+  # MARK: - View Life Cycle
+  @objc_method
+  def viewDidLoad(self):
+    send_super(__class__, self, 'viewDidLoad')
+    self.navigationItem.title = localizedString('DefaultToolBarTitle') if (
+      title := self.navigationItem.title) is None else title
+
+    self.view.backgroundColor = UIColor.systemBackgroundColor()
+
+    # See the `UIBarStyle` enum for more styles, including `.Default`.
+    self.navigationController.toolbar.setBarStyle_(UIBarStyle.black)
+    self.navigationController.toolbar.setTranslucent_(False)
+    self.navigationController.toolbar.setTintColor_(UIColor.systemGreenColor())
+    self.navigationController.toolbar.setBackgroundColor_(
+      UIColor.systemBlueColor())
+
+    # MARK: - `UIBarButtonItem` Creation and Configuration
+    refreshBarButtonItem = UIBarButtonItem.alloc().initWithBarButtonSystemItem(
+      UIBarButtonSystemItem.refresh,
+      target=self,
+      action=SEL('barButtonItemClicked:'))
+
+    # Note that there's no target/action since this represents empty space.
+    flexibleSpaceBarButtonItem = UIBarButtonItem.alloc(
+    ).initWithBarButtonSystemItem(UIBarButtonSystemItem.flexibleSpace,
+                                  target=None,
+                                  action=None)
+
+    actionBarButtonItem = UIBarButtonItem.alloc().initWithBarButtonSystemItem(
+      UIBarButtonSystemItem.action,
+      target=self,
+      action=SEL('actionBarButtonItemClicked:'))
+
+    toolbarButtonItems = [
+      refreshBarButtonItem,
+      flexibleSpaceBarButtonItem,
+      actionBarButtonItem,
+    ]
+    self.setToolbarItems_animated_(toolbarButtonItems, True)
+    self.navigationController.setToolbarHidden_(False)
+
+  @objc_method
+  def viewWillAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewWillAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print('viewWillAppear')
+
+  @objc_method
+  def viewDidAppear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidAppear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print('viewDidAppear')
+
+  @objc_method
+  def viewDidDisappear_(self, animated: bool):
+    send_super(__class__,
+               self,
+               'viewDidDisappear:',
+               animated,
+               argtypes=[
+                 ctypes.c_bool,
+               ])
+    #print('viewDidDisappear')
+
+  @objc_method
+  def didReceiveMemoryWarning(self):
+    send_super(__class__, self, 'didReceiveMemoryWarning')
+    print(f'{__class__}: didReceiveMemoryWarning')
+
+  # MARK: - Actions
+  @objc_method
+  def barButtonItemClicked_(self, barButtonItem):
+    print(
+      f'A bar button item on the tinted toolbar was clicked: {barButtonItem}.')
+
+  @objc_method
+  def actionBarButtonItemClicked_(self, barButtonItem):
+    print('h')
+
+
+if __name__ == '__main__':
+  from rbedge.functions import NSStringFromClass
+  from rbedge.enumerations import UIModalPresentationStyle
+  from rbedge import present_viewController
+
+  main_vc = TintedToolbarViewController.new()
+  _title = NSStringFromClass(TintedToolbarViewController)
+  main_vc.navigationItem.title = _title
+
+  presentation_style = UIModalPresentationStyle.fullScreen
+  present_viewController(main_vc, presentation_style)
+
