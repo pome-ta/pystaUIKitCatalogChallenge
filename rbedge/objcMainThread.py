a482ad8312b0a6ce2a355f77cfe9de3109e57bb2
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-02-04 10:51:13 +0900

    up

--- rbedge/objcMainThread.py
+++ rbedge/objcMainThread.py
@@ -26,12 +26,8 @@ def onMainThread(func):
 
   @functools.wraps(func)
   def wrapper(*args, **kwargs):
-    print(NSThread.isMainThread)
     if NSThread.isMainThread:
       func(*args, **kwargs)
-    
-    #func(*args, **kwargs)
-    
     block = Block(functools.partial(func, *args, **kwargs), None)
     libobjc.dispatch_async(dispatch_get_main_queue(), block)
 
