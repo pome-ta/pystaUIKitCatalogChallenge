5d81e8078b05e46548cef21a5a4e1e89d471cf59
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-02-04 10:52:33 +0900

    rebase

--- stackViewController.py
+++ stackViewController.py
@@ -2,7 +2,7 @@
   note: Storyboard 実装なし
   todo:
     - 時々落ちる、稀に無限クラッシュ
-    - 不要な呼び出しを整理
+    - 不要な呼び出しを整理
     - `viewDidLoad` 肥大化問題
 """
 import ctypes
@@ -27,7 +27,6 @@ from rbedge.enumerations import (
 from rbedge.globalVariables import (
   UIFontTextStyle, )
 
-from rbedge.functions import NSStringFromClass
 from rbedge.functions import arc4random_uniform
 from rbedge import pdbr
 
@@ -51,43 +50,39 @@ UIView = ObjCClass('UIView')
 
 
 class StackViewController(UIViewController):
-
+  
   @objc_method
   def dealloc(self):
     # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    print(f'\t- {NSStringFromClass(__class__)}: dealloc')
-
-  @objc_method
-  def loadView(self):
-    send_super(__class__, self, 'loadView')
-    print(f'\t{NSStringFromClass(__class__)}: loadView')
-
+    # print('\tdealloc')
+    pass
+  
   # MARK: - View Life Cycle
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
     self.navigationItem.title = localizedString('StackViewsTitle') if (
-      title := self.navigationItem.title) is None else title
-
+                                                                        title := self.navigationItem.title) is None else title
+    
     self.view.backgroundColor = UIColor.systemBackgroundColor()
-
-    # xxx: あとで、`setup` 的なのを作る？
+    
+    # xxx: あとで、`setup` 的なのを作る?
     # --- Symbols
     plusSymbol = UIImage.systemImageNamed('plus')
     minusSymbol = UIImage.systemImageNamed('minus')
-
+    
     touchUpInside = UIControlEvents.touchUpInside
-
+    
     # --- showingHidingStackView
     showingHidingStackView = UIStackView.alloc()
-
+    
     # --- --- showingHidingLabel
     showingHidingLabel = UILabel.new()
     showingHidingLabel.text = 'Showing/hiding views'
     showingHidingLabel.textAlignment = NSTextAlignment.center
     showingHidingLabel.setFont_(
       UIFont.preferredFontForTextStyle_(UIFontTextStyle.headline))
-
+    
     # --- --- / detailStackView
     detailStackView = UIStackView.alloc()
     # --- --- ---- detailLabel
@@ -100,7 +95,7 @@ class StackViewController(UIViewController):
     detailLabel.lineBreakMode = NSLineBreakMode.byTruncatingTail
     detailLabel.setFont_(
       UIFont.preferredFontForTextStyle_(UIFontTextStyle.body))
-
+    
     # --- --- ---- detailTextField
     detailTextField = UITextField.new()
     detailTextField.setContentHuggingPriority_forAxis_(
@@ -108,14 +103,14 @@ class StackViewController(UIViewController):
     detailTextField.borderStyle = UITextBorderStyle.roundedRect
     detailTextField.setFont_(UIFont.systemFontOfSize_(14.0))
     detailTextField.font.systemMinimumFontSize = 17.0
-
+    
     # --- --- ---- detailPlusButton
     detailPlusButton = UIButton.buttonWithType_(UIButtonType.system)
     detailPlusButton.setImage_forState_(plusSymbol, UIControlState.normal)
     detailPlusButton.contentEdgeInsets = UIEdgeInsetsMake(0.0, 10.0, 0.0, 10.0)
     detailPlusButton.addTarget_action_forControlEvents_(
       self, SEL('showFurtherDetail:'), touchUpInside)
-
+    
     # --- --- arrangedSubviews
     detailStackView.initWithArrangedSubviews_([
       detailLabel,
@@ -124,7 +119,7 @@ class StackViewController(UIViewController):
     ])
     detailStackView.spacing = 10.0
     # --- --- detailStackView /
-
+    
     # --- --- / furtherStackView
     furtherStackView = UIStackView.alloc()
     # --- --- --- furtherlLabel
@@ -137,7 +132,7 @@ class StackViewController(UIViewController):
     furtherlLabel.lineBreakMode = NSLineBreakMode.byTruncatingTail
     furtherlLabel.setFont_(
       UIFont.preferredFontForTextStyle_(UIFontTextStyle.body))
-
+    
     # --- --- --- furtherTextField
     furtherTextField = UITextField.new()
     furtherTextField.setContentHuggingPriority_forAxis_(
@@ -145,7 +140,7 @@ class StackViewController(UIViewController):
     furtherTextField.borderStyle = UITextBorderStyle.roundedRect
     furtherTextField.setFont_(UIFont.systemFontOfSize_(14.0))
     furtherTextField.font.systemMinimumFontSize = 17.0
-
+    
     # --- --- --- furtherMinusButton
     furtherMinusButton = UIButton.buttonWithType_(UIButtonType.system)
     furtherMinusButton.setImage_forState_(minusSymbol, UIControlState.normal)
@@ -153,7 +148,7 @@ class StackViewController(UIViewController):
       0.0, 10.0, 0.0, 10.0)
     furtherMinusButton.addTarget_action_forControlEvents_(
       self, SEL('hideFurtherDetail:'), touchUpInside)
-
+    
     # --- --- arrangedSubviews
     furtherStackView.initWithArrangedSubviews_([
       furtherlLabel,
@@ -162,13 +157,13 @@ class StackViewController(UIViewController):
     ])
     furtherStackView.spacing = 10.0
     # --- --- furtherStackView /
-
+    
     footerLabel = UILabel.new()
     footerLabel.text = 'Footer Label'
     footerLabel.textAlignment = NSTextAlignment.center
     footerLabel.setFont_(
       UIFont.preferredFontForTextStyle_(UIFontTextStyle.footnote))
-
+    
     # --- --- arrangedSubviews
     showingHidingStackView.initWithArrangedSubviews_([
       showingHidingLabel,
@@ -178,7 +173,7 @@ class StackViewController(UIViewController):
     ])
     showingHidingStackView.axis = UILayoutConstraintAxis.vertical
     showingHidingStackView.spacing = 10.0
-
+    
     # --- addRemoveStackView
     addRemoveStackView = UIStackView.alloc()
     # --- --- ---- addRemoveLabel
@@ -191,7 +186,7 @@ class StackViewController(UIViewController):
     addRemoveLabel.textAlignment = NSTextAlignment.center
     addRemoveLabel.setFont_(
       UIFont.preferredFontForTextStyle_(UIFontTextStyle.headline))
-
+    
     # --- --- ---- addbutton
     addbutton = UIButton.buttonWithType_(UIButtonType.system)
     addbutton.setImage_forState_(plusSymbol, UIControlState.normal)
@@ -200,7 +195,7 @@ class StackViewController(UIViewController):
     addbutton.contentEdgeInsets = UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0)
     addbutton.addTarget_action_forControlEvents_(
       self, SEL('addArrangedSubviewToStack:'), touchUpInside)
-
+    
     # --- --- ---- removebutton
     removebutton = UIButton.buttonWithType_(UIButtonType.system)
     removebutton.setImage_forState_(minusSymbol, UIControlState.normal)
@@ -209,32 +204,32 @@ class StackViewController(UIViewController):
     removebutton.contentEdgeInsets = UIEdgeInsetsMake(10.0, 10.0, 10.0, 10.0)
     removebutton.addTarget_action_forControlEvents_(
       self, SEL('removeArrangedSubviewFromStack:'), touchUpInside)
-
+    
     # --- --- arrangedSubviews
     addRemoveStackView.initWithArrangedSubviews_([
       addRemoveLabel,
       addbutton,
       removebutton,
     ])
-
+    
     # --- addRemoveExampleStackView
     addRemoveExampleStackView = UIStackView.new()
     addRemoveExampleStackView.axis = UILayoutConstraintAxis.vertical
     addRemoveExampleStackView.alignment = UIStackViewAlignment.center
     addRemoveExampleStackView.spacing = 10.0
-
+    
     # --- Layout
     self.view.addSubview_(showingHidingStackView)
     self.view.addSubview_(addRemoveStackView)
     self.view.addSubview_(addRemoveExampleStackView)
-
+    
     showingHidingStackView.translatesAutoresizingMaskIntoConstraints = False
     addRemoveStackView.translatesAutoresizingMaskIntoConstraints = False
     addRemoveExampleStackView.translatesAutoresizingMaskIntoConstraints = False
-
+    
     layoutMarginsGuide = self.view.layoutMarginsGuide
     safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
-
+    
     # --- showingHidingStackView
     NSLayoutConstraint.activateConstraints_([
       showingHidingStackView.leadingAnchor.constraintEqualToAnchor_(
@@ -244,13 +239,13 @@ class StackViewController(UIViewController):
       showingHidingStackView.topAnchor.constraintEqualToAnchor_constant_(
         safeAreaLayoutGuide.topAnchor, 8.0),
     ])
-
+    
     # todo: `detailLabel` と`furtherlLabel` の幅を連動
     NSLayoutConstraint.activateConstraints_([
       detailLabel.widthAnchor.constraintEqualToAnchor_(
         furtherlLabel.widthAnchor),
     ])
-
+    
     # --- addRemoveStackView
     NSLayoutConstraint.activateConstraints_([
       addRemoveStackView.topAnchor.constraintEqualToAnchor_constant_(
@@ -260,7 +255,7 @@ class StackViewController(UIViewController):
       addRemoveStackView.trailingAnchor.constraintEqualToAnchor_(
         layoutMarginsGuide.trailingAnchor),
     ])
-
+    
     # --- addRemoveExampleStackView
     NSLayoutConstraint.activateConstraints_([
       addRemoveExampleStackView.topAnchor.constraintEqualToAnchor_constant_(
@@ -271,15 +266,15 @@ class StackViewController(UIViewController):
         layoutMarginsGuide.trailingAnchor),
       # addRemoveExampleStackView.heightAnchor.constraintEqualToConstant_(42.0),  # xxx: `placeholder="YES"` ?
     ])
-
+    
     self.plusButton = detailPlusButton
     self.furtherDetailStackView = furtherStackView
     self.addRemoveExampleStackView = addRemoveExampleStackView
     self.addArrangedViewButton = addbutton
     self.removeArrangedViewButton = removebutton
-
+    
     self.maximumArrangedSubviewCount = 3
-
+  
   # MARK: - View Life Cycle
   @objc_method
   def viewWillAppear_(self, animated: bool):
@@ -293,7 +288,7 @@ class StackViewController(UIViewController):
     self.furtherDetailStackView.setHidden_(True)
     self.plusButton.setHidden_(False)
     self.updateAddRemoveButtons()
-
+  
   @objc_method
   def viewDidAppear_(self, animated: bool):
     send_super(__class__,
@@ -304,7 +299,7 @@ class StackViewController(UIViewController):
                  ctypes.c_bool,
                ])
     # print('viewDidAppear')
-
+  
   @objc_method
   def viewWillDisappear_(self, animated: bool):
     send_super(__class__,
@@ -315,7 +310,7 @@ class StackViewController(UIViewController):
                  ctypes.c_bool,
                ])
     # print('viewWillDisappear')
-
+  
   @objc_method
   def viewDidDisappear_(self, animated: bool):
     send_super(__class__,
@@ -326,29 +321,29 @@ class StackViewController(UIViewController):
                  ctypes.c_bool,
                ])
     # print('viewDidDisappear')
-
+  
   @objc_method
   def didReceiveMemoryWarning(self):
     send_super(__class__, self, 'didReceiveMemoryWarning')
     print(f'{__class__}: didReceiveMemoryWarning')
-
+  
   # MARK: - Actions
   @objc_method
   def showFurtherDetail_(self, _):
     # Animate the changes by performing them in a `UIViewPropertyAnimator` animation block.
-
+    
     @Block
     def animationsBlock() -> None:
       # Reveal the further details stack view and hide the plus button.
       self.furtherDetailStackView.setHidden_(False)
       self.plusButton.setHidden_(True)
-
+    
     showDetailAnimator = UIViewPropertyAnimator.alloc(
     ).initWithDuration_curve_animations_(NSTimeInterval(0.25),
                                          UIViewAnimationCurve.easeIn,
                                          animationsBlock)
     showDetailAnimator.startAnimation()
-
+  
   @objc_method
   def hideFurtherDetail_(self, _):
     # Animate the changes by performing them in a `UIViewPropertyAnimator` animation block.
@@ -357,44 +352,44 @@ class StackViewController(UIViewController):
       # Reveal the further details stack view and hide the plus button.
       self.furtherDetailStackView.setHidden_(True)
       self.plusButton.setHidden_(False)
-
+    
     hideDetailAnimator = UIViewPropertyAnimator.alloc(
     ).initWithDuration_curve_animations_(NSTimeInterval(0.25),
                                          UIViewAnimationCurve.easeOut,
                                          animationsBlock)
     hideDetailAnimator.startAnimation()
-
+  
   @objc_method
   def addArrangedSubviewToStack_(self, _):
     # Create a simple, fixed-size, square view to add to the stack view.
     newViewSize = CGSizeMake(38.0, 38.0)
     newView = UIView.alloc().initWithFrame_(CGRect(NSZeroPoint, newViewSize))
-
+    
     newView.backgroundColor = self.randomColor()
-
+    
     NSLayoutConstraint.activateConstraints_([
       newView.widthAnchor.constraintEqualToConstant_(newViewSize.width),
       newView.heightAnchor.constraintEqualToConstant_(newViewSize.height),
     ])
     # Adding an arranged subview automatically adds it as a child of the stack view.
     self.addRemoveExampleStackView.addArrangedSubview_(newView)
-
+    
     self.updateAddRemoveButtons()
-
+  
   @objc_method
   def removeArrangedSubviewFromStack_(self, _):
     # Make sure there is an arranged view to remove.
     if (viewToRemove :=
-        self.addRemoveExampleStackView.arrangedSubviews.lastObject()) is None:
+    self.addRemoveExampleStackView.arrangedSubviews.lastObject()) is None:
       return
-
+    
     self.addRemoveExampleStackView.removeArrangedSubview_(viewToRemove)
     # Calling `removeArrangedSubview` does not remove the provided view from the stack view's `subviews` array. Since we no longer want the view we removed to appear, we have to explicitly remove it from its superview.
     # `removeArrangedSubview` を呼び出しても、スタック ビューの `subviews` 配列から指定されたビューは削除されません。削除したビューを表示したくないので、スーパービューから明示的に削除する必要があります。
     viewToRemove.removeFromSuperview()
-
+    
     self.updateAddRemoveButtons()
-
+  
   # MARK: - Convenience
   @objc_method
   def updateAddRemoveButtons(self):
@@ -402,7 +397,7 @@ class StackViewController(UIViewController):
     self.addArrangedViewButton.setEnabled_(
       arrangedSubviewCount < self.maximumArrangedSubviewCount)
     self.removeArrangedViewButton.setEnabled_(arrangedSubviewCount > 0)
-
+  
   @objc_method
   def randomColor(self):
     red = arc4random_uniform(255) / 255.0
@@ -412,21 +407,13 @@ class StackViewController(UIViewController):
 
 
 if __name__ == '__main__':
-  #from rbedge.app import App
-  from rbedge.lifeCycle import loop
   from rbedge.functions import NSStringFromClass
   from rbedge.enumerations import UIModalPresentationStyle
-  from rbedge.app import present_viewController
-
+  from rbedge import present_viewController
+  
   main_vc = StackViewController.new()
   _title = NSStringFromClass(StackViewController)
   main_vc.navigationItem.title = _title
-
+  
   presentation_style = UIModalPresentationStyle.fullScreen
-
-  #app = App(main_vc)
-  #app.main_loop(presentation_style)
   present_viewController(main_vc, presentation_style)
-  loop.run_forever()
-  loop.close()
-
