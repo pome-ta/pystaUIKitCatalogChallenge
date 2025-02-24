25526474cdfcab4df6b93d1de74b10ec5eaa7496
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-02-25 00:08:38 +0900

    activityIndicatorViewController.py

--- activityIndicatorViewController.py
+++ activityIndicatorViewController.py
@@ -67,6 +67,7 @@ class ActivityIndicatorViewController(BaseTableViewController):
     send_super(__class__, self, 'viewDidLoad')
     #print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
 
+    # --- Navigation
     self.navigationItem.title = localizedString('ActivityIndicatorsTitle') if (
       title := self.navigationItem.title) is None else title
 
