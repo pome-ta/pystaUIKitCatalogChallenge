21f5af9d4c31422200e2ef3f751a0640d852546e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-02-25 00:06:01 +0900

    customPageControlViewController.py

--- customPageControlViewController.py
+++ customPageControlViewController.py
@@ -7,7 +7,7 @@ todo:
 import ctypes
 
 from pyrubicon.objc.api import ObjCClass
-from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super, SEL
 from pyrubicon.objc.types import CGRectMake
 
@@ -16,6 +16,8 @@ from rbedge.enumerations import (
   UIPageControlBackgroundStyle,
 )
 from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
+
 from pyLocalizedString import localizedString
 
 UIViewController = ObjCClass('UIViewController')
@@ -65,32 +67,32 @@ images = [
 
 
 class CustomPageControlViewController(UIViewController):
-  
+
+  pageControl: UIView = objc_property()
+  colorView: UIView = objc_property()
+
   @objc_method
   def dealloc(self):
     # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    # print('\tdealloc')
-    pass
-  
+    print(f'\t- {NSStringFromClass(__class__)}: dealloc')
+
   @objc_method
   def viewDidLoad(self):
-    send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
+    send_super(__class__, self, 'viewDidLoad')
+
     # --- Navigation
-    title = NSStringFromClass(__class__)
-    # self.navigationItem.title = title
-    self.navigationItem.title = localizedString('CustomPageControlTitle')
-    
+    self.navigationItem.title = localizedString('CustomPageControlTitle') if (
+      title := self.navigationItem.title) is None else title
     self.view.backgroundColor = UIColor.systemBackgroundColor()
-    
+
     # xxx: あとで切り出す
-    # self.pageControl = UIPageControl.alloc().init().autorelease()
     self.pageControl = UIPageControl.new()
     self.colorView: UIView = UIView.new()
-    
+
     self.setlayout()
     self.configurePageControl()
     self.pageControlValueDidChange()
-  
+
   @objc_method
   def viewWillAppear_(self, animated: bool):
     send_super(__class__,
@@ -101,7 +103,7 @@ class CustomPageControlViewController(UIViewController):
                  ctypes.c_bool,
                ])
     # print('viewWillAppear')
-  
+
   @objc_method
   def viewDidAppear_(self, animated: bool):
     send_super(__class__,
@@ -112,7 +114,7 @@ class CustomPageControlViewController(UIViewController):
                  ctypes.c_bool,
                ])
     # print('viewDidAppear')
-  
+
   @objc_method
   def viewWillDisappear_(self, animated: bool):
     send_super(__class__,
@@ -123,7 +125,7 @@ class CustomPageControlViewController(UIViewController):
                  ctypes.c_bool,
                ])
     # print('viewWillDisappear')
-  
+
   @objc_method
   def viewDidDisappear_(self, animated: bool):
     send_super(__class__,
@@ -134,12 +136,12 @@ class CustomPageControlViewController(UIViewController):
                  ctypes.c_bool,
                ])
     # print('viewDidDisappear')
-  
+
   @objc_method
   def didReceiveMemoryWarning(self):
     send_super(__class__, self, 'didReceiveMemoryWarning')
     print(f'{__class__}: didReceiveMemoryWarning')
-  
+
   @objc_method
   def setlayout(self):
     safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
@@ -147,10 +149,10 @@ class CustomPageControlViewController(UIViewController):
     self.pageControl.frame = CGRectMake(16.0, 639.5, 343.0, 27.5)
     self.colorView.frame = CGRectMake(40.0, 79.0, 295.0, 548.0)
     # self.colorView.backgroundColor = UIColor.systemMintColor()
-    
+
     self.view.addSubview_(self.pageControl)
     self.view.addSubview_(self.colorView)
-    
+
     # --- layout
     self.pageControl.translatesAutoresizingMaskIntoConstraints = False
     NSLayoutConstraint.activateConstraints_([
@@ -161,7 +163,7 @@ class CustomPageControlViewController(UIViewController):
       self.pageControl.bottomAnchor.constraintEqualToAnchor_(
         safeAreaLayoutGuide.bottomAnchor),
     ])
-    
+
     self.colorView.translatesAutoresizingMaskIntoConstraints = False
     NSLayoutConstraint.activateConstraints_([
       self.colorView.trailingAnchor.constraintEqualToAnchor_constant_(
@@ -173,11 +175,11 @@ class CustomPageControlViewController(UIViewController):
       self.colorView.bottomAnchor.constraintEqualToAnchor_constant_(
         safeAreaLayoutGuide.bottomAnchor, -40),
     ])
-  
+
   @objc_method
   def configurePageControl(self):
     # wip: よしなに要素を配置したい
-    
+
     # The total number of available pages is based on the number of available colors.
     # 利用可能なページの総数は、利用可能な色の数に基づいています。
     self.pageControl.setNumberOfPages_(len(colors))
@@ -185,21 +187,21 @@ class CustomPageControlViewController(UIViewController):
     # self.pageControl.setPageIndicatorTintColor_(UIColor.systemGreenColor())
     self.pageControl.setCurrentPageIndicatorTintColor_(
       UIColor.systemPurpleColor())
-    
+
     # Prominent background style.
     # 目立つ背景スタイル。
     self.pageControl.backgroundStyle = UIPageControlBackgroundStyle.prominent
-    
+
     # Set custom indicator images.
     # カスタムインジケータイメージを設定します。
     for idx, img in enumerate(images):
       self.pageControl.setIndicatorImage_forPage_(img, idx)
     # xxx: 要素範囲確認用
     # self.pageControl.setBackgroundColor_(UIColor.systemDarkRedColor())
-    
+
     self.pageControl.addTarget_action_forControlEvents_(
       self, SEL('pageControlValueDidChange'), UIControlEvents.valueChanged)
-  
+
   # MARK: - Actions
   @objc_method
   def pageControlValueDidChange(self):
@@ -212,10 +214,17 @@ class CustomPageControlViewController(UIViewController):
 
 
 if __name__ == '__main__':
+  from rbedge.app import App
+
   from rbedge.enumerations import UIModalPresentationStyle
-  from rbedge import present_viewController
-  from rbedge import pdbr
-  
-  pc_vc = CustomPageControlViewController.new()
-  style = UIModalPresentationStyle.fullScreen
-  present_viewController(pc_vc, style)
+
+  main_vc = CustomPageControlViewController.new()
+  _title = NSStringFromClass(CustomPageControlViewController)
+  main_vc.navigationItem.title = _title
+
+  #presentation_style = UIModalPresentationStyle.fullScreen
+  presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc)
+  app.main_loop(presentation_style)
+
