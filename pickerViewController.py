214a36e621f70eac621cfcf42deca4c412587502
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-02-27 02:59:50 +0900

    pickerViewController.py

--- pickerViewController.py
+++ pickerViewController.py
@@ -5,12 +5,15 @@ import ctypes
 from enum import IntEnum, auto
 
 from pyrubicon.objc.api import ObjCClass
-from pyrubicon.objc.api import objc_method
+from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super, objc_id
+from pyrubicon.objc.types import NSInteger
 
 from rbedge.globalVariables import NSAttributedStringKey
 
+from rbedge.functions import NSStringFromClass
 from rbedge import pdbr
+
 from pyLocalizedString import localizedString
 
 UIViewController = ObjCClass('UIViewController')
@@ -36,43 +39,50 @@ class ColorComponent(IntEnum):
 
 
 class PickerViewController(UIViewController):
-  
+
+  colorSwatchPickerView: UIPickerView = objc_property()
+  colorSwatchView: UIView = objc_property()
+  numberOfColorValuesPerComponent: NSInteger = objc_property()
+  redColor: float = objc_property()
+  greenColor: float = objc_property()
+  blueColor: float = objc_property()
+  mutableAttributedString: NSMutableAttributedString = objc_property()
+
   @objc_method
   def dealloc(self):
     # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    # print('\tdealloc')
-    pass
-  
+    print(f'\t- {NSStringFromClass(__class__)}: dealloc')
+
   # MARK: - View Life Cycle
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
     self.navigationItem.title = localizedString('PickerViewTitle') if (
-                                                                        title := self.navigationItem.title) is None else title
+      title := self.navigationItem.title) is None else title
     self.view.backgroundColor = UIColor.systemBackgroundColor()
-    
+
     # --- pickerView
     # todo: 変数名`pickerView` だと、関数名に干渉する
     colorSwatchPickerView = UIPickerView.new()
     colorSwatchPickerView.dataSource = self
     colorSwatchPickerView.delegate = self
-    
+
     # --- colorSwatchView
     colorSwatchView = UIView.new()
-    
+
     # --- Layout
     safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
     layoutMarginsGuide = self.view.layoutMarginsGuide
-    
+
     self.view.addSubview_(colorSwatchPickerView)
     colorSwatchPickerView.translatesAutoresizingMaskIntoConstraints = False
     self.view.addSubview_(colorSwatchView)
     colorSwatchView.translatesAutoresizingMaskIntoConstraints = False
-    
+
     NSLayoutConstraint.activateConstraints_([
       colorSwatchPickerView.widthAnchor.constraintEqualToConstant_(375.0),
     ])
-    
+
     NSLayoutConstraint.activateConstraints_([
       colorSwatchView.trailingAnchor.constraintEqualToAnchor_constant_(
         safeAreaLayoutGuide.trailingAnchor, -20.0),
@@ -87,21 +97,21 @@ class PickerViewController(UIViewController):
       colorSwatchView.leadingAnchor.constraintEqualToAnchor_constant_(
         safeAreaLayoutGuide.leadingAnchor, 20.0),
     ])
-    
+
     self.colorSwatchPickerView = colorSwatchPickerView
     self.colorSwatchView = colorSwatchView
-    
+
     self.numberOfColorValuesPerComponent = int(RGB.max / RGB.offset) + 1
-    
+
     self.redColor = RGB.min
     self.greenColor = RGB.min
     self.blueColor = RGB.min
-    
+
     # todo: `pickerView_attributedTitleForRow_forComponent_` 内で定義すると落ちる
     self.mutableAttributedString = NSMutableAttributedString.alloc()
-    
+
     self.configurePickerView()
-  
+
   @objc_method
   def viewWillAppear_(self, animated: bool):
     send_super(__class__,
@@ -111,8 +121,8 @@ class PickerViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewWillAppear')
-  
+    #print(f'\t{NSStringFromClass(__class__)}: viewWillAppear_')
+
   @objc_method
   def viewDidAppear_(self, animated: bool):
     send_super(__class__,
@@ -122,8 +132,8 @@ class PickerViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewDidAppear')
-  
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
+
   @objc_method
   def viewWillDisappear_(self, animated: bool):
     send_super(__class__,
@@ -133,8 +143,8 @@ class PickerViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewWillDisappear')
-  
+    # print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
+
   @objc_method
   def viewDidDisappear_(self, animated: bool):
     send_super(__class__,
@@ -144,18 +154,19 @@ class PickerViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewDidDisappear')
-  
+    print(f'\t{NSStringFromClass(__class__)}: viewDidDisappear_')
+
   @objc_method
   def didReceiveMemoryWarning(self):
     send_super(__class__, self, 'didReceiveMemoryWarning')
     print(f'{__class__}: didReceiveMemoryWarning')
-  
+
   @objc_method
   def updateColorSwatchViewBackgroundColor(self):
     self.colorSwatchView.backgroundColor = UIColor.colorWithRed_green_blue_alpha_(
-      self.redColor, self.greenColor, self.blueColor, 1.0)
-  
+      self.redColor.floatValue, self.greenColor.floatValue,
+      self.blueColor.floatValue, 1.0)
+
   @objc_method
   def configurePickerView(self):
     # Set the default selected rows (the desired rows to initially select will vary from app to app).
@@ -165,7 +176,7 @@ class PickerViewController(UIViewController):
       ColorComponent.green: 41,
       ColorComponent.blue: 24,
     }
-    
+
     for colorComponent, selectedRow in selectedRows.items():
       """
       Note that the delegate method on `UIPickerViewDelegate` is not triggered
@@ -180,18 +191,18 @@ class PickerViewController(UIViewController):
       self.pickerView(self.colorSwatchPickerView,
                       didSelectRow=selectedRow,
                       inComponent=int(colorComponent))
-  
+
   # MARK: - UIPickerViewDataSource
   @objc_method
-  def numberOfComponentsInPickerView_(self, pickerView) -> int:
+  def numberOfComponentsInPickerView_(self, pickerView) -> NSInteger:
     return len(ColorComponent)
-  
+
   @objc_method
-  def pickerView_numberOfRowsInComponent_(self, component) -> int:
-    return self.numberOfColorValuesPerComponent
-  
+  def pickerView_numberOfRowsInComponent_(self, component) -> NSInteger:
+    return self.numberOfColorValuesPerComponent.intValue
+
   # MARK: - UIPickerViewDelegate
-  
+
   @objc_method
   def pickerView_attributedTitleForRow_forComponent_(self, pickerView,
                                                      row: int, component: int):
@@ -201,21 +212,21 @@ class PickerViewController(UIViewController):
     redColorComponent = RGB.min
     greenColorComponent = RGB.min
     blueColorComponent = RGB.min
-    
+
     if component == ColorComponent.red:
       redColorComponent = value
     if component == ColorComponent.green:
       greenColorComponent = value
     if component == ColorComponent.blue:
       blueColorComponent = value
-    
+
     if redColorComponent < 0.5:
       redColorComponent = 0.5
     if blueColorComponent < 0.5:
       blueColorComponent = 0.5
     if greenColorComponent < 0.5:
       greenColorComponent = 0.5
-    
+
     foregroundColor = UIColor.colorWithRed_green_blue_alpha_(
       redColorComponent, greenColorComponent, blueColorComponent, 1.0)
     # Set the foreground color for the entire attributed string.
@@ -224,11 +235,11 @@ class PickerViewController(UIViewController):
     ], [
       NSAttributedStringKey.foregroundColor,
     ])
-    
+
     title = self.mutableAttributedString.initWithString_attributes_(
       f'{int(colorValue)}', attributes)
     return title
-  
+
   @objc_method
   def pickerView_didSelectRow_inComponent_(self, pickerView, row: int,
                                            component: int):
@@ -240,7 +251,7 @@ class PickerViewController(UIViewController):
     if component == ColorComponent.blue:
       self.blueColor = colorComponentValue
     self.updateColorSwatchViewBackgroundColor()
-  
+
   # MARK: - UIPickerViewAccessibilityDelegate
   def pickerView_accessibilityLabelForComponent_(self, pickerView,
                                                  component: int) -> objc_id:
@@ -253,14 +264,16 @@ class PickerViewController(UIViewController):
 
 
 if __name__ == '__main__':
-  from rbedge.functions import NSStringFromClass
+  from rbedge.app import App
   from rbedge.enumerations import UIModalPresentationStyle
-  from rbedge import present_viewController
-  
+
   main_vc = PickerViewController.new()
-  
   _title = NSStringFromClass(PickerViewController)
   main_vc.navigationItem.title = _title
-  
-  presentation_style = UIModalPresentationStyle.fullScreen
-  present_viewController(main_vc, presentation_style)
+
+  #presentation_style = UIModalPresentationStyle.fullScreen
+  presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc, presentation_style)
+  app.present()
+
