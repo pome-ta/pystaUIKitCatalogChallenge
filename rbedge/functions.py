2744ca0706ee7611cfefbac4834d1f37b3c3735d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-03-25 21:53:35 +0900

    functions

--- rbedge/functions.py
+++ rbedge/functions.py
@@ -5,13 +5,16 @@ from pyrubicon.objc.types import CGSize, CGFloat, with_preferred_encoding, __LP6
 from pyrubicon.objc.runtime import libc, Foundation, Class, load_library, objc_id
 
 UIKit = load_library('UIKit')
+CoreGraphics = load_library('CoreGraphics')
 
 
 def NSStringFromClass(cls: Class) -> ObjCInstance:
-  _NSStringFromClass = Foundation.NSStringFromClass
-  _NSStringFromClass.restype = ctypes.c_void_p
-  _NSStringFromClass.argtypes = [Class]
-  return ObjCInstance(_NSStringFromClass(cls))
+  _function = Foundation.NSStringFromClass
+  _function.restype = ctypes.c_void_p
+  _function.argtypes = [
+    Class,
+  ]
+  return ObjCInstance(_function(cls))
 
 
 if __LP64__:
@@ -47,26 +50,48 @@ def arc4random_uniform(value: ctypes.c_uint32) -> int:
 # ref: [UIGraphicsBeginImageContextWithOptions | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uigraphicsbeginimagecontextwithoptions(_:_:_:)?language=objc)
 def UIGraphicsBeginImageContextWithOptions(size: CGSize, opaque: bool,
                                            scale: CGFloat) -> ObjCInstance:
-  _fnc = UIKit.UIGraphicsBeginImageContextWithOptions
-  _fnc.restype = ctypes.c_void_p
-  _fnc.argtypes = [
+  _function = UIKit.UIGraphicsBeginImageContextWithOptions
+  _function.restype = ctypes.c_void_p
+  _function.argtypes = [
     CGSize,
     ctypes.c_bool,
     CGFloat,
   ]
-  return ObjCInstance(_fnc(size, opaque, scale))
+  return ObjCInstance(_function(size, opaque, scale))
+
 
 # ref: [UIGraphicsGetImageFromCurrentImageContext | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uigraphicsgetimagefromcurrentimagecontext()?language=objc)
 def UIGraphicsGetImageFromCurrentImageContext() -> ObjCInstance:
-  _fnc = UIKit.UIGraphicsGetImageFromCurrentImageContext
-  _fnc.restype = objc_id
-  _fnc.argtypes = []
-  return ObjCInstance(_fnc())
+  _function = UIKit.UIGraphicsGetImageFromCurrentImageContext
+  _function.restype = objc_id
+  _function.argtypes = []
+  return ObjCInstance(_function())
+
 
 # ref: [UIGraphicsEndImageContext | Apple Developer Documentation](https://developer.apple.com/documentation/uikit/uigraphicsendimagecontext()?language=objc)
-def UIGraphicsEndImageContext():
-  _fnc = UIKit.UIGraphicsEndImageContext
-  _fnc.restype = ctypes.c_void_p
-  _fnc.argtypes = []
-  _fnc()
+def UIGraphicsEndImageContext() -> None:
+  _function = UIKit.UIGraphicsEndImageContext
+  _function.restype = ctypes.c_void_p
+  _function.argtypes = []
+  _function()
+
+
+# ref: [CGImageGetDataProvider | Apple Developer Documentation](https://developer.apple.com/documentation/coregraphics/cgimage/dataprovider?language=objc)
+def CGImageGetDataProvider(image: ctypes.c_void_p) -> ObjCInstance:
+  _function = CoreGraphics.CGImageGetDataProvider
+  _function.restype = ctypes.c_void_p
+  _function.argtypes = [
+    ctypes.c_void_p,
+  ]
+  return ObjCInstance(_function(image))
+
+
+# ref: [CGDataProviderCopyData | Apple Developer Documentation](https://developer.apple.com/documentation/coregraphics/cgdataprovider/data?language=objc)
+def CGDataProviderCopyData(provider: ObjCInstance) -> ObjCInstance:
+  _function = CoreGraphics.CGDataProviderCopyData
+  _function.restype = ctypes.c_void_p
+  _function.argtypes = [
+    ctypes.c_void_p,
+  ]
+  return ObjCInstance(_function(provider))
 
