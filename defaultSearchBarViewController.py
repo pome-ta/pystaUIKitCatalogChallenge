0c502d4d544999d2c56f3e16de1d2cca09cb01bd
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-01 13:30:46 +0900

    defaultSearchBarViewController.py

--- defaultSearchBarViewController.py
+++ defaultSearchBarViewController.py
@@ -30,7 +30,9 @@ class DefaultSearchBarViewController(UIViewController):
 
   @objc_method
   def viewDidLoad(self):
-    send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
+    send_super(__class__, self, 'viewDidLoad')
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
+    
     # --- Navigation
     self.navigationItem.title = localizedString('DefaultSearchBarTitle') if (
       title := self.navigationItem.title) is None else title
