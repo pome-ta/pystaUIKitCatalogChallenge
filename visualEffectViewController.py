93374f013b27bef5aee571253d275ea5a1e5fbd1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-01 10:57:43 +0900

    visualEffectViewController.py

--- visualEffectViewController.py
+++ visualEffectViewController.py
@@ -13,11 +13,12 @@ from rbedge.enumerations import (
   UILayoutConstraintAxis,
 )
 from rbedge.globalVariables import UIFontTextStyle
-
 from rbedge.pythonProcessUtils import dataWithContentsOfURL
 
-from rbedge import pdbr
 from pyLocalizedString import localizedString
+
+from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
 
 UIViewController = ObjCClass('UIViewController')
 NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
@@ -35,23 +36,27 @@ UIFontDescriptor = ObjCClass('UIFontDescriptor')
 
 
 class VisualEffectViewController(UIViewController):
-  
+
   @objc_method
   def dealloc(self):
     # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    # print('\tdealloc')
-    pass
-  
+    print(f'\t- {NSStringFromClass(__class__)}: dealloc')
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+    #print(f'\t{NSStringFromClass(__class__)}: loadView')
+
   # MARK: - View Life Cycle
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
     self.navigationItem.title = localizedString('VisualEffectTitle') if (
-                                                                          title := self.navigationItem.title) is None else title
+      title := self.navigationItem.title) is None else title
     self.view.backgroundColor = UIColor.systemBackgroundColor()
-    
+
     image_path = './UIKitCatalogCreatingAndCustomizingViewsAndControls/UIKitCatalog/Assets.xcassets/Flowers_2.imageset/Flowers_2.png'
-    
+
     imageReference = UIImage.alloc().initWithData_(
       dataWithContentsOfURL(image_path))
     imageView = UIImageView.alloc().initWithImage_(imageReference)
@@ -59,12 +64,12 @@ class VisualEffectViewController(UIViewController):
       251.0, UILayoutConstraintAxis.horizontal)
     imageView.setContentHuggingPriority_forAxis_(
       251.0, UILayoutConstraintAxis.vertical)
-    
+
     # xxx: 関数で返すと落ちるので
     visualEffect = UIVisualEffectView.alloc().initWithEffect_(
       UIBlurEffect.effectWithStyle_(UIBlurEffectStyle.regular))
     visualEffect.translatesAutoresizingMaskIntoConstraints = False
-    
+
     # xxx: 関数で返すと落ちるので
     textView = UITextView.new()
     textView.setFont_(UIFont.systemFontOfSize_(14.0))
@@ -72,16 +77,16 @@ class VisualEffectViewController(UIViewController):
     textView.translatesAutoresizingMaskIntoConstraints = False
     textView.backgroundColor = UIColor.clearColor
     if (fontDescriptor :=
-    UIFontDescriptor.preferredFontDescriptorWithTextStyle_(
-      UIFontTextStyle.body).fontDescriptorWithSymbolicTraits_(
-      UIFontDescriptorSymbolicTraits.traitLooseLeading)):
+        UIFontDescriptor.preferredFontDescriptorWithTextStyle_(
+          UIFontTextStyle.body).fontDescriptorWithSymbolicTraits_(
+            UIFontDescriptorSymbolicTraits.traitLooseLeading)):
       looseLeadingFont = UIFont.fontWithDescriptor_size_(fontDescriptor, 0.0)
       textView.setFont_(looseLeadingFont)
-    
+
     # --- Layout
     layoutMarginsGuide = self.view.layoutMarginsGuide
     safeAreaLayoutGuide = self.view.safeAreaLayoutGuide
-    
+
     self.view.addSubview_(imageView)
     imageView.translatesAutoresizingMaskIntoConstraints = False
     NSLayoutConstraint.activateConstraints_([
@@ -92,7 +97,7 @@ class VisualEffectViewController(UIViewController):
       imageView.centerYAnchor.constraintEqualToAnchor_(
         safeAreaLayoutGuide.centerYAnchor),
     ])
-    
+
     self.view.addSubview_(visualEffect)
     visualEffect.translatesAutoresizingMaskIntoConstraints = False
     NSLayoutConstraint.activateConstraints_([
@@ -104,7 +109,7 @@ class VisualEffectViewController(UIViewController):
       visualEffect.bottomAnchor.constraintEqualToAnchor_(
         imageView.bottomAnchor),
     ])
-    
+
     self.view.addSubview_(textView)
     textView.translatesAutoresizingMaskIntoConstraints = False
     NSLayoutConstraint.activateConstraints_([
@@ -117,14 +122,14 @@ class VisualEffectViewController(UIViewController):
       textView.bottomAnchor.constraintEqualToAnchor_(
         visualEffect.safeAreaLayoutGuide.bottomAnchor),
     ])
-    
+
     if True:  # wip: `available(iOS 15, *)`
       # Use UIToolTipInteraction which is available on iOS 15 or later, add it to the image view.
       toolTipString = localizedString('VisualEffectToolTipTitle')
       interaction = UIToolTipInteraction.alloc().initWithDefaultToolTip_(
         toolTipString)
       imageView.addInteraction_(interaction)
-  
+
   @objc_method
   def viewWillAppear_(self, animated: bool):
     send_super(__class__,
@@ -134,8 +139,8 @@ class VisualEffectViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewWillAppear')
-  
+    #print(f'\t{NSStringFromClass(__class__)}: viewWillAppear_')
+
   @objc_method
   def viewDidAppear_(self, animated: bool):
     send_super(__class__,
@@ -145,8 +150,8 @@ class VisualEffectViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewDidAppear')
-  
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
+
   @objc_method
   def viewWillDisappear_(self, animated: bool):
     send_super(__class__,
@@ -156,8 +161,8 @@ class VisualEffectViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewWillDisappear')
-  
+    # print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
+
   @objc_method
   def viewDidDisappear_(self, animated: bool):
     send_super(__class__,
@@ -167,22 +172,25 @@ class VisualEffectViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewDidDisappear')
-  
+    print(f'\t{NSStringFromClass(__class__)}: viewDidDisappear_')
+
   @objc_method
   def didReceiveMemoryWarning(self):
     send_super(__class__, self, 'didReceiveMemoryWarning')
-    print(f'{__class__}: didReceiveMemoryWarning')
+    print(f'\t{NSStringFromClass(__class__)}: didReceiveMemoryWarning')
 
 
 if __name__ == '__main__':
-  from rbedge.functions import NSStringFromClass
+  from rbedge.app import App
   from rbedge.enumerations import UIModalPresentationStyle
-  from rbedge import present_viewController
-  
+
   main_vc = VisualEffectViewController.new()
   _title = NSStringFromClass(VisualEffectViewController)
   main_vc.navigationItem.title = _title
-  
-  presentation_style = UIModalPresentationStyle.fullScreen
-  present_viewController(main_vc, presentation_style)
+
+  #presentation_style = UIModalPresentationStyle.fullScreen
+  presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc, presentation_style)
+  app.present()
+
