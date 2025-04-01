c51e5dca30d9c874896ffebbef74d14ca3cb322b
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-01 13:30:53 +0900

    fontPickerViewController.py

--- fontPickerViewController.py
+++ fontPickerViewController.py
@@ -55,6 +55,9 @@ class FontPickerViewController(UIViewController):
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
+
+    # --- Navigation
     self.navigationItem.title = localizedString('FontPickerTitle') if (
       title := self.navigationItem.title) is None else title
     self.view.backgroundColor = UIColor.systemBackgroundColor()
