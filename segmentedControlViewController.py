508ca758417336e92a028841e624fe6874593c10
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-01 13:31:29 +0900

    segmentedControlViewController.py

--- segmentedControlViewController.py
+++ segmentedControlViewController.py
@@ -91,8 +91,10 @@ class SegmentedControlViewController(BaseTableViewController):
   # MARK: - View Life Cycle
   @objc_method
   def viewDidLoad(self):
-    send_super(__class__, self, 'viewDidLoad')  # xxx: 不要?
-
+    send_super(__class__, self, 'viewDidLoad')
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
+    
+    # --- Navigation
     self.navigationItem.title = localizedString('SegmentedControlsTitle') if (
       title := self.navigationItem.title) is None else title
 
