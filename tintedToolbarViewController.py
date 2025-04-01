d12cb4df3d42854b3162201f3014621a4bc019c1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-01 10:52:35 +0900

    tintedToolbarViewController.py

--- tintedToolbarViewController.py
+++ tintedToolbarViewController.py
@@ -5,7 +5,7 @@ import ctypes
 
 from pyrubicon.objc.api import ObjCClass
 from pyrubicon.objc.api import objc_method
-from pyrubicon.objc.runtime import send_super, objc_id, SEL
+from pyrubicon.objc.runtime import send_super, SEL
 
 from rbedge.enumerations import (
   UIBarStyle,
@@ -13,9 +13,10 @@ from rbedge.enumerations import (
 )
 from rbedge.pythonProcessUtils import dataWithContentsOfURL
 
-from rbedge import pdbr
-
 from pyLocalizedString import localizedString
+
+from rbedge.functions import NSStringFromClass
+from rbedge import pdbr
 
 UIViewController = ObjCClass('UIViewController')
 NSLayoutConstraint = ObjCClass('NSLayoutConstraint')
@@ -29,62 +30,67 @@ UIActivityViewController = ObjCClass('UIActivityViewController')
 
 
 class TintedToolbarViewController(UIViewController):
-  
+
   @objc_method
   def dealloc(self):
     # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
-    # print('\tdealloc')
-    pass
-  
+    print(f'\t- {NSStringFromClass(__class__)}: dealloc')
+    #self.navigationController.setToolbarHidden_animated_(True, True)
+
+  @objc_method
+  def loadView(self):
+    send_super(__class__, self, 'loadView')
+    #print(f'\t{NSStringFromClass(__class__)}: loadView')
+
   # MARK: - View Life Cycle
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
     self.navigationItem.title = localizedString('TintedToolbarTitle') if (
-                                                                           title := self.navigationItem.title) is None else title
-    
+      title := self.navigationItem.title) is None else title
+
     self.view.backgroundColor = UIColor.systemBackgroundColor()
-    
+
     _navToolbar = self.navigationController.toolbar
     toolbar = UIToolbar.alloc().initWithFrame_(_navToolbar.frame)
     toolbar.setAutoresizingMask_(_navToolbar.autoresizingMask)
-    
+
     toolbarAppearance = UIToolbarAppearance.new()
     toolbarAppearance.configureWithDefaultBackground()
     # toolbarAppearance.configureWithOpaqueBackground()
     # toolbarAppearance.configureWithTransparentBackground()
     toolbarAppearance.setBackgroundColor_(UIColor.systemBlueColor())
-    
+
     toolbar.standardAppearance = toolbarAppearance
     toolbar.scrollEdgeAppearance = toolbarAppearance
     toolbar.compactAppearance = toolbarAppearance
     toolbar.compactScrollEdgeAppearance = toolbarAppearance
-    
+
     self.navigationController.setToolbar_(toolbar)
-    
+
     # See the `UIBarStyle` enum for more styles, including `.Default`.
     toolbar.setBarStyle_(UIBarStyle.black)
     toolbar.setTranslucent_(False)
     toolbar.setTintColor_(UIColor.systemGreenColor())
     toolbar.setBackgroundColor_(UIColor.systemBlueColor())
-    
+
     # MARK: - `UIBarButtonItem` Creation and Configuration
     refreshBarButtonItem = UIBarButtonItem.alloc().initWithBarButtonSystemItem(
       UIBarButtonSystemItem.refresh,
       target=self,
       action=SEL('barButtonItemClicked:'))
-    
+
     # Note that there's no target/action since this represents empty space.
     flexibleSpaceBarButtonItem = UIBarButtonItem.alloc(
     ).initWithBarButtonSystemItem(UIBarButtonSystemItem.flexibleSpace,
                                   target=None,
                                   action=None)
-    
+
     actionBarButtonItem = UIBarButtonItem.alloc().initWithBarButtonSystemItem(
       UIBarButtonSystemItem.action,
       target=self,
       action=SEL('actionBarButtonItemClicked:'))
-    
+
     toolbarButtonItems = [
       refreshBarButtonItem,
       flexibleSpaceBarButtonItem,
@@ -92,7 +98,7 @@ class TintedToolbarViewController(UIViewController):
     ]
     self.setToolbarItems_animated_(toolbarButtonItems, True)
     self.navigationController.setToolbarHidden_animated_(False, False)
-  
+
   @objc_method
   def viewWillAppear_(self, animated: bool):
     send_super(__class__,
@@ -102,8 +108,8 @@ class TintedToolbarViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewWillAppear')
-  
+    #print(f'\t{NSStringFromClass(__class__)}: viewWillAppear_')
+
   @objc_method
   def viewDidAppear_(self, animated: bool):
     send_super(__class__,
@@ -113,8 +119,8 @@ class TintedToolbarViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewDidAppear')
-  
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidAppear_')
+
   @objc_method
   def viewWillDisappear_(self, animated: bool):
     send_super(__class__,
@@ -124,9 +130,9 @@ class TintedToolbarViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewWillDisappear')
-    self.navigationController.setToolbarHidden_animated_(True, True)
-  
+    # print(f'\t{NSStringFromClass(__class__)}: viewWillDisappear_')
+    #self.navigationController.setToolbarHidden_animated_(True, False)
+
   @objc_method
   def viewDidDisappear_(self, animated: bool):
     send_super(__class__,
@@ -136,24 +142,24 @@ class TintedToolbarViewController(UIViewController):
                argtypes=[
                  ctypes.c_bool,
                ])
-    # print('viewDidDisappear')
-  
+    print(f'\t{NSStringFromClass(__class__)}: viewDidDisappear_')
+
   @objc_method
   def didReceiveMemoryWarning(self):
     send_super(__class__, self, 'didReceiveMemoryWarning')
     print(f'{__class__}: didReceiveMemoryWarning')
-  
+
   # MARK: - Actions
   @objc_method
   def barButtonItemClicked_(self, barButtonItem):
     print(
       f'A bar button item on the tinted toolbar was clicked: {barButtonItem}.')
-  
+
   @objc_method
   def actionBarButtonItemClicked_(self, barButtonItem):
     image_path = './UIKitCatalogCreatingAndCustomizingViewsAndControls/UIKitCatalog/Assets.xcassets/Flowers_1.imageset/Flowers_1.png'
     if (image :=
-    UIImage.alloc().initWithData_(dataWithContentsOfURL(image_path))):
+        UIImage.alloc().initWithData_(dataWithContentsOfURL(image_path))):
       activityItems = [
         'Shared piece of text',
         image,
@@ -161,7 +167,7 @@ class TintedToolbarViewController(UIViewController):
       activityViewController = UIActivityViewController.alloc(
       ).initWithActivityItems_applicationActivities_(activityItems, None)
       if (popoverPresentationController :=
-      activityViewController.popoverPresentationController()) is not None:
+          activityViewController.popoverPresentationController()) is not None:
         # xxx: 挙動未確認
         popoverPresentationController.barButtonItem = barButtonItem
       self.presentViewController(activityViewController,
@@ -170,13 +176,16 @@ class TintedToolbarViewController(UIViewController):
 
 
 if __name__ == '__main__':
-  from rbedge.functions import NSStringFromClass
+  from rbedge.app import App
   from rbedge.enumerations import UIModalPresentationStyle
-  from rbedge import present_viewController
-  
+
   main_vc = TintedToolbarViewController.new()
   _title = NSStringFromClass(TintedToolbarViewController)
   main_vc.navigationItem.title = _title
-  
-  presentation_style = UIModalPresentationStyle.fullScreen
-  present_viewController(main_vc, presentation_style)
+
+  #presentation_style = UIModalPresentationStyle.fullScreen
+  presentation_style = UIModalPresentationStyle.pageSheet
+
+  app = App(main_vc, presentation_style)
+  app.present()
+
