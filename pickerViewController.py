3cfcd0ce909fb51bbdd9021412e7055c431aac1a
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-01 13:31:15 +0900

    pickerViewController.py

--- pickerViewController.py
+++ pickerViewController.py
@@ -57,6 +57,9 @@ class PickerViewController(UIViewController):
   @objc_method
   def viewDidLoad(self):
     send_super(__class__, self, 'viewDidLoad')
+    #print(f'\t{NSStringFromClass(__class__)}: viewDidLoad')
+
+    # --- Navigation
     self.navigationItem.title = localizedString('PickerViewTitle') if (
       title := self.navigationItem.title) is None else title
     self.view.backgroundColor = UIColor.systemBackgroundColor()
