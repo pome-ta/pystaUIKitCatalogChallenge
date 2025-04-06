ef070caa290cb2919989d45ad115811af55b5c3e
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-06 18:26:04 +0900

    caseElement.py

--- caseElement.py
+++ caseElement.py
@@ -1,3 +1,4 @@
+from pyrubicon.objc.api import ObjCInstance
 from pyrubicon.objc.api import NSObject, NSString
 from pyrubicon.objc.api import objc_method, objc_property
 from pyrubicon.objc.runtime import send_super
@@ -16,19 +17,22 @@ class CaseElement(NSObject):
   configHandlerName: NSString = objc_property()
 
   @objc_method
-  def dealloc(self):
+  def dealloc(self) -> None:
     # xxx: 呼ばない-> `send_super(__class__, self, 'dealloc')`
     print(f'\t\t- {NSStringFromClass(__class__)}: dealloc')
 
   @objc_method
-  def initWithTitle_cellID_configHandlerName_(self, title, cellID,
-                                              configHandlerName):
+  def initWithTitle_cellID_configHandlerName_(
+      self, title, cellID, configHandlerName) -> ObjCInstance:
+    send_super(__class__, self, 'init')
+    #print(f'\t{NSStringFromClass(__class__)}: initWithTitle_cellID_configHandlerName_')
+
     self.title = title
     self.cellID = cellID
     self.configHandlerName = configHandlerName
     return self
 
   @objc_method
-  def targetView(self, cell):
+  def targetView(self, cell) -> ObjCInstance:
     return cell.contentView.subviews()[0] if cell is not None else None
 
