66d9b7a1f1b54a156fb117c567ae2a0dffd1e22c
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-04-06 13:25:51 +0900

    update

---
+++ UIKitCatalogCreatingAndCustomizingViewsAndControls/UIKitCatalog/AppDelegate.swift
@@ -0,0 +1,17 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The application-specific delegate class.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
